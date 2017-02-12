db3d17952fbd5096b57b007e196176ce9c5f226e
YGL <2369015621@qq.com>
2017-02-12 18:56:55 +0800

    SunshineWeather first commit

---
+++ app/src/main/java/com/example/ygl/sunshineweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.ygl.sunshineweather.model;
+
+/**
+ * Created by YGL on 2017/2/12.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
